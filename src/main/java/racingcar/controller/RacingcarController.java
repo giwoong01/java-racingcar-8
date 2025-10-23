4c7c219bb94df6c18222e3effc5728c5dca24fe6
giwoong <dev.choi0409@gmail.com>
2025-10-23 13:08:28 +0900

    feat: 시도할 횟수 입력 기능 구현
    - TryCount 도메인 구현
    - 시소할 횟수 0 또는 음수, 문자인 경우 예외 처리

--- src/main/java/racingcar/controller/RacingcarController.java
+++ src/main/java/racingcar/controller/RacingcarController.java
@@ -1,15 +1,25 @@
 package racingcar.controller;
 
 import racingcar.domain.Cars;
+import racingcar.domain.TryCount;
 import racingcar.view.InputView;
 
 public class RacingcarController {
 
     public void run() {
+        Cars cars = getCars();
+        TryCount tryCount = getTryCount();
+
+    }
+
+    private Cars getCars() {
         String carNames = InputView.inputCarNames();
-        Cars cars = new Cars(carNames);
+        return new Cars(carNames);
+    }
 
-
+    private TryCount getTryCount() {
+        String tryCount = InputView.inputTryCount();
+        return new TryCount(tryCount);
     }
 
 }
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -12,4 +12,9 @@ public final class InputView {
         return Console.readLine();
     }
 
+    public static String inputTryCount() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        return Console.readLine();
+    }
+
 }
