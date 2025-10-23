d3645dacbec0db6b91dee4ad5eb534ae912f0281
giwoong <dev.choi0409@gmail.com>
2025-10-23 17:48:42 +0900

    feat: Console 종료

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,15 +1,20 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
 import racingcar.controller.RacingcarController;
 import racingcar.domain.power.RandomPowerGenerator;
 
 public class Application {
 
     public static void main(String[] args) {
-        RacingcarController racingcarController = new RacingcarController(
-                new RandomPowerGenerator()
-        );
-        racingcarController.run();
+        try {
+            RacingcarController racingcarController = new RacingcarController(
+                    new RandomPowerGenerator()
+            );
+            racingcarController.run();
+        } finally {
+            Console.close();
+        }
     }
 
 }
